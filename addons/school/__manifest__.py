3acb64ea15f0fa44586b9e9084278223a6d86e46
Miguel <159189630+MiguelAntonioRS@users.noreply.github.com>
2025-01-10 10:43:53 -0500

    Cambiando las propiedades del modulo

--- addons/school/__manifest__.py
+++ addons/school/__manifest__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "school",
+    'name': "Escuela",
 
     'summary': """
         Short (1 phrase/line) summary of the module's purpose, used as
@@ -10,13 +10,13 @@
         Long description of module's purpose
     """,
 
-    'author': "My Company",
-    'website': "https://www.yourcompany.com",
+    'author': "Miguel",
+    'website': "https://www.migueldev.com",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/16.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Uncategorized',
+    'category': 'education',
     'version': '0.1',
 
     # any module necessary for this one to work correctly
