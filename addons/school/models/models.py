0760266fe9af657f4e68f31b73eb9c810af762c0
Miguel <159189630+MiguelAntonioRS@users.noreply.github.com>
2025-01-11 22:02:56 -0500

    Crreando un formulario

--- addons/school/models/models.py
+++ addons/school/models/models.py
@@ -1,10 +1,12 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models, fields
+from odoo import models,fields
 
 
 class Profesor(models.Model):
     _name = 'school.profesor'
     _description = 'profesor'
 
-    name = fields.Char()
+    name = fields.Char(required=True)
+    description = fields.Text()
+    age = fields.Integer(required=True)
