b40de2d9e8d98c740427ca994ea0769daee7cafc
Miguel <159189630+MiguelAntonioRS@users.noreply.github.com>
2025-01-13 22:00:17 -0500

    Terminado

--- addons/school/models/models.py
+++ addons/school/models/models.py
@@ -21,7 +21,7 @@ class Profesor(models.Model):
         default="universidad",
         required=True,
     )
-    student = fields.One2many("school.profesor", inverse_name="profesor")
+    student = fields.One2many("school.profesor", inverse_name="profesor", string="Profesor")
     
 class Student(models.Model):
     _name = 'school.student'
