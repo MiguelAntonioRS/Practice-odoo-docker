c0291e81380247b7b420e5e2279080d70bfab0e1
Miguel <159189630+MiguelAntonioRS@users.noreply.github.com>
2025-01-18 12:55:56 -0500

    Creando el modulo reporte_pdf

---
+++ addons/reporte_pdf/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': "Reporte PDF contacto",
+    'version': "1.0",
+    'category': "Custom",
+    'author': "OpenCode",
+    'depends': ['base'],
+    'data': [
+        'views/report_action.xml',
+        'report/contact_report_template.xml',
+    ],
+    'installable': True,
+    'application': False,
+    'license': 'LGPL-3',
+}
